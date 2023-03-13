447c3a6996977a0e77e0dcbb115c4821807a5f7a
Samuel <tolotrasam@gmail.com>
2023-03-13 18:04:42 -0400

    initial commit

---
+++ android/app/src/main/kotlin/com/example/test_overlay_flutter_to_delete/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_overlay_flutter_to_delete
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_overlay_window/flutter_overlay_window.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MessangerChatHead extends StatefulWidget {
+  const MessangerChatHead({Key? key}) : super(key: key);
+
+  @override
+  State<MessangerChatHead> createState() => _MessangerChatHeadState();
+}
+
+class _MessangerChatHeadState extends State<MessangerChatHead> {
+  Color color = const Color(0xFFFFFFFF);
+  BoxShape shape = BoxShape.rectangle;
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterOverlayWindow.overlayListener.listen((event) {
+      if (event == 'HEY') {
+        // setState(() {
+        //  Update Timer
+        // });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      elevation: 0.0,
+      child: GestureDetector(
+        onTap: () async {
+          if (shape == BoxShape.circle) {
+            await FlutterOverlayWindow.resizeOverlay(matchParent, matchParent);
+            setState(() {
+              shape = BoxShape.rectangle;
+            });
+          } else {
+            await FlutterOverlayWindow.resizeOverlay(150, 150);
+            setState(() {
+              shape = BoxShape.circle;
+            });
+          }
+        },
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(color: color, shape: shape),
+          child: const Center(
+            child: FlutterLogo(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+@pragma("vm:entry-point")
+void overlayMain() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MessangerChatHead(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  Future<void> showOverLay(BuildContext context) async {
+    /// check if overlay permission is granted
+    final bool status = await FlutterOverlayWindow.isPermissionGranted();
+
+    /// request overlay permission
+    /// it will open the overlay settings page and return `true` once the permission granted.
+    if (status == false) {
+      final bool? status = await FlutterOverlayWindow.requestPermission();
+      if (status == false) {
+        print("permission not granted");
+        return;
+      }
+    }
+    print("showing overlay");
+    await FlutterOverlayWindow.showOverlay(
+      height: 150,
+      width: 150,
+      enableDrag: true,
+      alignment: OverlayAlignment.topRight,
+    );
+    // await FlutterOverlayWindow.resizeOverlay(30, 30);
+
+    return;
+  }
+
+  void _incrementCounter() {
+    showOverLay(context);
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
